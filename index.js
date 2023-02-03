908710ce0fbbcbc5ef3be3703cea89bffb33c00e
rafaelcptx <rafaelteixeiradesigner@gmail.com>
2023-02-03 18:55:51 -0300

    adicionada fetch da pag HTML para o DB

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let app = express();
 let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb://localhost:8001/Filmes", {
+  .connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -14,16 +15,38 @@ mongoose
 
 app.use(express.static(__dirname + "/public"));
 
+// ===========================================================================================================
+
+const Schema = mongoose.Schema;
+
+const filmeSchema = new Schema({
+  filme: String,
+  nota: Number,
+});
+
+const Filme = mongoose.model("Filme", filmeSchema);
+
 app
   .route("/")
   .get((req, res) => {
     res.sendFile(__dirname + "/index.html");
   })
   .post((req, res) => {
-    let { filme, nota } = req.body;
+    let { nome, nota } = req.body;
+    let filme = new Filme({ filme: nome, nota: nota });
 
-    console.log(filme);
-    console.log(nota);
+    filme.save((err, data) => {
+      if (err) console.log(err);
+      console.log(data);
+    });
+
+    res.redirect("/");
   });
+
+app.get("/filmes", (req, res) => {
+  Filme.find({}, (err, documents) => {
+    res.json(documents);
+  });
+});
 
 app.listen(8001, console.log("Servidor rodando com sucesso!"));
